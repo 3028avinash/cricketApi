84f0f3138349f3a089f63f1573d2fac9a04fea79
3028avinash <85918644+3028avinash@users.noreply.github.com>
2023-05-05 12:23:05 +0530

    Update fantasies

--- db/migrate/20230501101617_create_fantasies.rb
+++ db/migrate/20230501101617_create_fantasies.rb
@@ -1,6 +1,6 @@
 class CreateFantasies < ActiveRecord::Migration[7.0]
   def change
-    drop_table :fantasies
+    # drop_table :fantasies
     create_table :fantasies do |t|
       t.string :title
       t.string :logo
